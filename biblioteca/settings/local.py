4ce7465e7b9b58084352db7d6dfc8daa82910bb4
juanogit <jcarayaf64@gmail.com>
2021-05-02 18:13:42 +0200

    se modifica las configuraciones del directorio settings, manage y wsgi

--- biblioteca/settings/local.py
+++ biblioteca/settings/local.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbbiblioteca',
+        'USER': 'juano',
+        'PASSWORD': '3M44r4ll44',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
