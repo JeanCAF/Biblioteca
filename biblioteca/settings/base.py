dc453e838c2f5b3d59ed0133df308744fc45d715
juanogit <jcarayaf64@gmail.com>
2021-05-02 19:23:31 +0200

    se crea los modelos de autor, libro, lector y se hace la primera migracion

--- biblioteca/settings/base.py
+++ biblioteca/settings/base.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local app
+    'applications.autor',
+    'applications.libro',
+    'applications.lector'
 ]
 
 MIDDLEWARE = [
